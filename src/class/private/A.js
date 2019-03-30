0ac06af76ddfb6d52f712fe46cb293c7b66dea68
chaingree <254756875@qq.com>
2019-03-30 20:21:43 +0800

    Static 私有测试

--- src/class/private/A.js
+++ src/class/private/A.js
@@ -6,27 +6,32 @@ const _ = privateCreater();
 /**
  * 包含公共属性和方法的类定义
  */
-class A{
-  constructor(){
-    _(this,_A)
+class A {
+  constructor() {
+    _(this, _A)
   }
 
-  log(s){
+  log(s) {
     // 打印私有化对象
-    console.log( _(this) )
+    console.log(_(this))
     _(this).log(s)
   }
-  
+
+  get PI(){
+    return _A.PI
+  }
+
 }
 
 
 /**
  * 包含私有属性和方法的类定义
  */
-class _A{
+class _A {
   count = 1
-  log(s){
-    console.log(this.count + ' ' + s )
+  static PI = '3.14'
+  log(s) {
+    console.log(this.count + ' ' + s)
     this.count++
   }
 }
